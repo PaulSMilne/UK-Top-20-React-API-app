611906ed76805bf68f3709c9a8cfc61296e54c55
Paul Milne <hallhill@gmail.com>
2017-01-11 00:47:45 +0000

    wrote an initial main container component for the app

---
+++ client/src/components/TopTwenty.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var TopTwenty = React.createClass(
+
+     render: function(){
+
+          return(
+               <section className='main-content'>
+                    <h1>UK Top Twenty</h1>
+               </section>
+          )
+     }
+
+)
