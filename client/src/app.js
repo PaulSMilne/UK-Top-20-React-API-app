c1e359bc5a44d1bd10262646ae5bf7f0f64fb887
Paul Milne <hallhill@gmail.com>
2017-01-11 00:43:28 +0000

    added jsx files and pointed main app file to the main react file

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,10 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var TopTwenty = require('./components/TopTwenty.jsx')
 
 window.onload = function(){
   ReactDOM.render(
-    <h1> App Started </h1>,
+    <TopTwenty />,
     document.getElementById('app')
   );
 }
