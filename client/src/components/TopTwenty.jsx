9953b9c93e098dc3e665e124e6d70065466b928f
Paul Milne <hallhill@gmail.com>
2017-01-11 06:40:34 +0000

    added a request to the iTunes top twenty api

--- client/src/components/TopTwenty.jsx
+++ client/src/components/TopTwenty.jsx
@@ -9,8 +9,15 @@ var TopTwenty = React.createClass({
      },
 
      componentDidMount: function() {
-          
-     }
+          var url = 'https://itunes.apple.com/gb/rss/topsongs/limit=20/json';
+          var request = new XMLHttpRequest();
+          request.open('GET', url);
+          request.onload = function(){
+            var data = JSON.parse(request.responseText);
+            this.setState({songs: data.feed.entry});
+          }.bind(this);
+          request.send();
+     },
 
      render: function(){
 
@@ -19,6 +26,7 @@ var TopTwenty = React.createClass({
                     <TopTwentyHeader title="UK Top 20" />
                </section>
           )
+          console.log(data);
      }
 
 });
